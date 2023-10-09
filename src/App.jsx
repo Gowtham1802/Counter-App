e6323b93c9342ae060831881ff045f7a801ebf23
Gowtham1802 <gowthama7575@gmail.com>
2023-10-09 14:37:28 +0530

    react codes add

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {BsFillArrowThroughHeartFill} from 'react-icons/bs'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BsFillArrowThroughHeartFill/>
+
+    </>
+  )
+}
+
+export default App
