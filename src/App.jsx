ca05d2f246cdce7e0cd549240c4a3dcd76e886a5
Gowtham1802 <gowthama7575@gmail.com>
2023-10-10 14:15:26 +0530

    add codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {BsFillArrowThroughHeartFill} from 'react-icons/bs'
+import { FiPlusCircle } from "react-icons/fi";
+import { AiFillMinusCircle } from "react-icons/ai";
+import { BsFillTrashFill } from "react-icons/bs";
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <BsFillArrowThroughHeartFill/>
+    <main className="w-screen h-80 bg-cyan-200 border-cyan-400 border-8 flex items-center justify-center flex-col">
+      <div>COUNT APP</div>
+      <div className="flex items-center justify-center gap-4 flex-col">
+        <div className="flex items-center justify-center flex-row gap-2 ">
+          <div className="w-12 h-6 bg-yellow-400 flex items-center justify-center">
+            Zero
+          </div>
+          <button className="w-8 h-6 bg-gray-500 flex items-center justify-center">
+            <FiPlusCircle></FiPlusCircle>
+          </button>
+          <button className="w-8 h-6 bg-cyan-500 flex items-center justify-center">
+            <AiFillMinusCircle></AiFillMinusCircle>
+          </button>
+          <button className="w-8 h-6 bg-red-500 flex items-center justify-center">
+            <BsFillTrashFill></BsFillTrashFill>
+          </button>
+        </div>
+        <div className="flex items-center justify-center flex-row gap-2">
+          <div className="w-12 h-6 bg-yellow-400 flex items-center justify-center">
+            Zero
+          </div>
+          <button className="w-8 h-6 bg-gray-500 flex items-center justify-center">
+            <FiPlusCircle></FiPlusCircle>
+          </button>
+          <button className="w-8 h-6 bg-cyan-500 flex items-center justify-center">
+            <AiFillMinusCircle></AiFillMinusCircle>
+          </button>
+          <button className="w-8 h-6 bg-red-500 flex items-center justify-center">
+            <BsFillTrashFill></BsFillTrashFill>
+          </button>
+        </div>
+        <div className="flex items-center justify-center flex-row gap-2">
+          <div className="w-12 h-6 bg-yellow-400 flex items-center justify-center">
+            Zero
+          </div>
+          <button className="w-8 h-6 bg-gray-500 flex items-center justify-center">
+            <FiPlusCircle></FiPlusCircle>
+          </button>
+          <button className="w-8 h-6 bg-cyan-500 flex items-center justify-center">
+            <AiFillMinusCircle></AiFillMinusCircle>
+          </button>
+          <button className="w-8 h-6 bg-red-500 flex items-center justify-center">
+            <BsFillTrashFill></BsFillTrashFill>
+          </button>
+        </div>
+        <div className="flex items-center justify-center flex-row gap-2">
+          <div className="w-12 h-6 bg-yellow-400 flex items-center justify-center">
+            Zero
+          </div>
+          <button className="w-8 h-6 bg-gray-500 flex items-center justify-center">
+            <FiPlusCircle></FiPlusCircle>
+          </button>
+          <button className="w-8 h-6 bg-cyan-500 flex items-center justify-center">
+            <AiFillMinusCircle></AiFillMinusCircle>
+          </button>
+          <button className="w-8 h-6 bg-red-500 flex items-center justify-center">
+            <BsFillTrashFill></BsFillTrashFill>
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
